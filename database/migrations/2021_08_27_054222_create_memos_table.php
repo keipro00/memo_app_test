47bfd2848bdfc334a0f202cbd5bbdd8f7dfcf6ce
keipro00 <keipro00@outlook.jp>
2021-08-27 15:39:55 +0900

    ログイン機能実装

--- database/migrations/2021_08_27_054222_create_memos_table.php
+++ database/migrations/2021_08_27_054222_create_memos_table.php
@@ -14,8 +14,16 @@ class CreateMemosTable extends Migration
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            // unsignedを使うことで−符号をなくす。第2引数にtrueを入れると勝手に増えていく
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義→deleted_atを自動生成
+            $table->softDeletes();
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
