12162155c7d90cfb9a06adefb560a78ba80950f2
keipro00 <keipro00@outlook.jp>
2021-08-28 14:00:35 +0900

    リファクタリング

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,24 +28,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //すべてのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function($view){
-            $query_tag = \Request::query('tag');
-            //もしクエリパラメーターtagがあれば
-            if(!empty($query_tag)){
-                $memos = Memo::select('memos.*')
-                    ->leftjoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
-                    ->where('memo_tags.tag_id', '=', $query_tag)
-                    ->where('user_id' , '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at','DESC') //ASC=小さい順
-                    ->get();
-            }else{
-                //タグで絞り込み、タグなければすべて取得
-            $memos = Memo::select('memos.*')
-                ->where('user_id' , '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at','DESC') //ASC=小さい順
-                ->get();
-            }
+    
+        // インスタンス化
+        $memo_model = new Memo();
+
+        $memos = $memo_model->getMyMemo();
 
         $tags = Tag::where('user_id', '=',\Auth::id())
         ->whereNull('deleted_at')
