8178e5fd69e83c6b6eae5a867fc94ed05aeec70c
keipro00 <keipro00@outlook.jp>
2021-08-28 13:42:53 +0900

    タグ絞り込み機能実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Models\Memo;
+use App\Models\Tag;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,13 +28,31 @@ class AppServiceProvider extends ServiceProvider
     {
         //すべてのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function($view){
-        $memos = Memo::select('memos.*')
-            ->where('user_id' , '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at','DESC') //ASC=小さい順
-            ->get();
+            $query_tag = \Request::query('tag');
+            //もしクエリパラメーターtagがあれば
+            if(!empty($query_tag)){
+                $memos = Memo::select('memos.*')
+                    ->leftjoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                    ->where('memo_tags.tag_id', '=', $query_tag)
+                    ->where('user_id' , '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at','DESC') //ASC=小さい順
+                    ->get();
+            }else{
+                //タグで絞り込み、タグなければすべて取得
+            $memos = Memo::select('memos.*')
+                ->where('user_id' , '=', \Auth::id())
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at','DESC') //ASC=小さい順
+                ->get();
+            }
 
-            $view->with('memos', $memos);
+        $tags = Tag::where('user_id', '=',\Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('updated_at','DESC') //ASC=小さい順
+        ->get();
+
+            $view->with('memos', $memos)->with('tags', $tags);
         });
     }
 }
